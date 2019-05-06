079e09a6218d4c125d1530997e55f4b35ac0c358
Jerh. O'Connor <jerocono@ie.ibm.com>
2019-05-06 13:39:34 +0100

    Adding RESTful endpoints

--- src/index.js
+++ src/index.js
@@ -5,20 +5,56 @@
 // Loading JSON from a file
 // Look at log4js basics
 
-
 import "dotenv/config";
 import cors from 'cors';
 import express from "express";
 
+import {users, messages} from './users';
+
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  return res.send("Recieved a GET HTTP method");
+});
+
+app.post('/', (req, res) => {
+  return res.send("Recieved a POST HTTP method");
+});
+
+app.put('/', (req, res) => {
+    return res.send("Recieved a PUT HTTP method");
+});
+
+app.delete('/', (req, res) => {
+    return res.send("recieved a DELETE HTTP method");
 });
 
 app.get("/test", (req, res) => {
   res.send("Test key is: " + process.env.MY_API_KEY);
+});
+
+// Manipulate the USER resource
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(Object.values(users[req.params.userId]));
+})
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 app.listen(process.env.PORT, () =>
